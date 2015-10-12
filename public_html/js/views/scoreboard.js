9538d4001cdb9a396d8a390c65237f76ad09d356
nastya <dyudina.anastasiya@yandex.ru>
2015-10-12 07:54:17 +0300

    stash

--- public_html/js/views/scoreboard.js
+++ public_html/js/views/scoreboard.js
@@ -11,30 +11,11 @@ define([
 ){
 
     var ScoreBoardView = Backbone.View.extend({
-        mainTemplate: scoreboard(),
-
-        initialize: function () {
-            //this.collection.bind("change reset add remove", this.render, this);
-            $.ajax({url: "utils/players-score.html",
-                    context: this,
-                    success: function(response) {
-                        this.playerTemplate = response;
-                        this.renderPlayerTemplate();
-                        }
-                    });
-        },
+        template: scoreboard,
 
         render: function () {
-            this.$el.html(this.mainTemplate);
-            if (this.playerTemplate) {
-                this.renderPlayerTemplate(); 
-            }
-        },
-
-        renderPlayerTemplate: function() {
             var players  = this.collection.toJSON();
-            var playersHtml = _.template(this.playerTemplate, {players: players});
-            $('.b-players__inner-list').html(playersHtml); 
+            this.$el.html(this.template(players));
         },
 
         events: {
@@ -54,7 +35,6 @@ define([
     var scoreBoardView = new ScoreBoardView({
         collection: playerCollection,
         el: $('.b-inner-main-window'),
-        //playerListClass: '.b-players__inner-list',
     });
 
     return scoreBoardView;
